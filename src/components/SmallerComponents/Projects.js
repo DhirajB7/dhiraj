ae106c0c5b62d6576e2e826c291f7ac603b09f08
dhiraj <d.basavaraju@thisisdmg.com>
2020-11-17 11:48:11 +0530

    Ready To Add Real Time Data

--- src/components/SmallerComponents/Projects.js
+++ src/components/SmallerComponents/Projects.js
@@ -1,43 +1,14 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
+import OneCard from "./OneCard";
+import dogImage from "../images/dog.jpg";
 
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
   cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
   },
 }));
 
@@ -46,31 +17,29 @@ function Projects() {
   return (
     <Container className={classes.cardGrid} maxWidth="md">
       <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} md={4}>
-          {/* 1st Project - START*/}
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.cardMedia}
-              image="https://source.unsplash.com/random"
-              title="Image title"
-            />
-            <CardContent className={classes.cardContent}>
-              <Typography gutterBottom variant="h5" component="h2">
-                Heading
-              </Typography>
-              <Typography>
-                This is a media card. You can use this section to describe the
-                content.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-                View
-              </Button>
-            </CardActions>
-          </Card>
-          {/* 1st Project - END*/}
-        </Grid>
+        <OneCard
+          heading="Image Heading"
+          description="Image Description Image Description Image Description Image Description"
+          imageUrl={dogImage}
+          imageTitle="random title"
+          githubUrl="https://dhirajb7.github.io"
+        />
+
+        <OneCard
+          heading="Image Heading"
+          description="Image Description Image Description Image Description Image Description"
+          imageUrl={dogImage}
+          imageTitle="random title"
+          githubUrl="https://dhirajb7.github.in"
+        />
+
+        <OneCard
+          heading="Image Heading"
+          description="Image Description Image Description Image Description Image Description"
+          imageUrl={dogImage}
+          imageTitle="random title"
+          githubUrl="https://dhirajb7.github.com"
+        />
       </Grid>
     </Container>
   );
