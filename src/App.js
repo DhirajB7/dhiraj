7da9ab14866711896d47b62a22997f373762fd5e
dhiraj <d.basavaraju@thisisdmg.com>
2021-01-03 08:36:26 +0530

    responsive for resize removed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import "./App.css";
-import React, { useState } from "react";
-import { CssBaseline, CircularProgress } from "@material-ui/core";
+import React from "react";
+import { CssBaseline } from "@material-ui/core";
 import MainContent from "./components/SmallerComponents/MainContent";
 import Projects from "./components/SmallerComponents/Projects";
 import Footer from "./components/SmallerComponents/Footer";
 
 function App() {
 
-	const [change,setChange] = useState(false);
-
-	window.onresize = () => {
-
-		setChange(true)
-
-		setTimeout(() => {
-			setChange(false)
-		}, 1500);
-	}
-
 	return (
-
-		change ? <CircularProgress id = "loader" size = "3.8rem" thickness = "5"/> :
 
 		<React.Fragment>
 
